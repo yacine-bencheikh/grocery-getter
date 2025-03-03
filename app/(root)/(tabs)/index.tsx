209b7be35382b614dab9cd8445897804cd1808f8
yassine ben cheikh <ybenchikh472@gmail.com>
2025-03-03 21:55:34 +0100

    homePage almost done

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,14 +1,48 @@
-import { Text,View } from "react-native";
-import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "~/constants/images";
+import icons from "~/constants/icons";
+import Search from "~/components/Search";
+import { Card, FeaturedCard } from "~/components/Cards";
 export default function Home() {
   return (
-  <View>
-    <Text className=" font-rubikBold">welcome to restate</Text>
-    <Link href="/sign-in">Sign in</Link>
-    <Link href="/explore">Explore</Link>
-    <Link href="/profile">profile</Link>
-    {/* <Link href="/properties/1">propertys</Link> */}
-    
-  </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                good morning
+              </Text>
+              <Text className="text-base font-rubikMedium text-black-300">
+                Adrian
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubikBold text-black-300">Featured</Text>
+            <TouchableOpacity><Text className="text-base font-rubikBold text-primary-300">See All</Text></TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubikBold text-black-300">Our Recommandation</Text>
+            <TouchableOpacity><Text className="text-base font-rubikBold text-primary-300">See All</Text></TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <Card/>
+            <Card/> 
+            
+          </View>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/Cards.tsx
@@ -0,0 +1,49 @@
+import { View, Text, Touchable, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import images from '~/constants/images'
+import icons from '~/constants/icons'
+
+export const Card = ({ onPress }: Props) => {
+    return (
+        <TouchableOpacity onPress={onPress} className='flex-1 w-full  px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative'>
+            <View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50'>
+                <Image source={icons.star} className='size-2.5'/>
+                <Text className='font-rubikBold text-primary-300 ml-0.5 text-xs'>4.77</Text>
+            </View>
+                <Image source={images.newYork} className='w-full h-36 rounded-lg  '/>
+
+            <View className='flex flex-col mt-2 '>
+                <Text className='text-base font-rubikBold text-black-300'>cozy studio</Text>
+                <Text className='text-xs font-rubik text-black-200'>22 W 15th St, New York </Text>
+                <View className='flex flex-row items-center justify-between mt-2'>
+                    <Text className='text-base font-rubikBold text-primary-300'>$2.500</Text>
+                    <Image source={icons.heart} className='w-5 h-5 mr-2' tintColor={"#191d31"} />
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+interface Props {
+    onPress?: () => void
+}
+
+export const FeaturedCard = ({ onPress }: Props) => {
+    return (
+        <TouchableOpacity onPress={onPress} className='flex flex-col items-start w-52 h-64 relative'>
+            <Image source={images.japan} className='size-full rounded-2xl' />
+            <Image source={images.cardGradient} className='size-full rounded-2xl absolute' />
+            <View className='flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute  top-5 right-5 '>
+                <Image source={icons.star} className='size-3.5'/>
+                <Text className='font-rubikBold text-primary-300 ml-1 text-xs'>4.77</Text>
+            </View>
+            <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
+                <Text className='text-xl font-rubikExtraBold text-white'>Modern Apartment</Text>
+                <Text className='text-base font-rubik text-white'>22 W 15th St, New York </Text>
+                <View className='flex flex-row items-center justify-between w-full'>
+                    <Text className='font-rubikExtraBold text-white text-xl'>$2.500</Text>
+                    <Image source={icons.heart} className='size-5' />
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
